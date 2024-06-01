cae8bbc074064c15d6f7afbab736dc8011a1b959
Eryk Sobczak <44040492+turbify@users.noreply.github.com>
2024-06-01 22:24:58 +0200

    textures, fonts

--- assets/js/name.js
+++ assets/js/name.js
@@ -4,11 +4,9 @@ function getName() {
 
     // Check if the user provided a name
     if (userName) {
-        // Store the name in a variable and display it back to the user
-        alert("Hello, " + userName + "!");
-        
-        // You can use the userName variable further in your script as needed
-        console.log("User name is: " + userName);
+
+        // Redirect to another HTML view
+        window.open('main.html?name=' + encodeURIComponent(userName));
     } else {
         alert("You didn't enter a name!");
     }
