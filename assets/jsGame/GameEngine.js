2a958e888106e73b2163bc65728a2504f1f4b795
Mikosusek <mikosus2000@gmail.com>
2024-06-01 22:25:07 +0200

    backup

---
+++ assets/jsGame/GameEngine.js
@@ -0,0 +1,4 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
