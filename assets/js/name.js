acc305f2373bdacfc8996de399afab92d7ccc9f1
Eryk Sobczak <44040492+turbify@users.noreply.github.com>
2024-06-03 00:05:11 +0200

    joining room, index fix

--- assets/js/name.js
+++
@@ -1,8 +0,0 @@
-function getName() {
-    var userName = document.getElementById('floatingInput').value;
-    if (userName) {
-        window.open('main.html?name=' + encodeURIComponent(userName));
-    } else {
-        alert("You didn't enter a name!");
-    }
-}
